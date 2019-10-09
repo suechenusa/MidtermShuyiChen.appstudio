4978e0f9b0c905ebc341bd1a05c1db7561224519
Shuyi Chen <suechenusa@gmail.com>
2019-10-09 16:31:56 -0500

    Finish Income Bracket
    Finish Income Bracket

--- forms/Calculation/Calculation.js
+++ forms/Calculation/Calculation.js
@@ -1,3 +1,4 @@
+/*
 //the function to calculator the formula
 function calcAvgSquare(num1,num2,num3){
    return ((num1 + num2 + num3)/3) * Math.pow(num1,2)
@@ -9,3 +10,4 @@ let num2 = Number(prompt("Enter your second number"))
 let num3 = Number(prompt("Enter your third number"))
 let answer = calcAvgSquare(num1,num2,num3)
 alert(`The answer using ${num1}, ${num2}, ${num3} is ${answer}`)
+*/
---
+++ forms/IncomeBracket/IncomeBracket.js
@@ -0,0 +1,17 @@
+
+btSubmit.onclick=function(){
+  let income = Number(InptIncome.value)
+  if (income<30000)
+    LResult.value=`With your income of ${income}, you are in a tax bracket of 8%`
+  else if (income>=30000&&income<99999)
+    LResult.value=`With your income of ${income}, you are in a tax bracket of 15%`
+  else if (income>=99999)
+    LResult.value=`With your income of ${income}, you are in a tax bracket of 25%"`
+  else
+    LResult.value=`The code have error`
+}
+
+btAgain.onclick=function(){
+  InptIncome.value=""
+  LResult.value=""
+}
